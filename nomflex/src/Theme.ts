ce99a77ad60b3548221df42b38deaeb6816e3a73
taehun <xun415@naver.com>
2022-06-12 20:40:51 +0900

    nomflex-start

---
+++ nomflex/src/Theme.ts
@@ -0,0 +1,14 @@
+// @ts-ignore
+import { DefaultTheme } from 'styled-components';
+export const defalutTheme: DefaultTheme = {
+    red: "#E51013",
+    black: {
+        veryDark: "#141414",
+        darker: "#181818",
+        lighter: "#2F2F2F",
+    },
+    white: {
+        lighter: "#fff",
+        darker: "#e5e5e5"
+    }
+};
